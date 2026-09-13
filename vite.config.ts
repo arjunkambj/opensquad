d476b79c15846616a5219706229e34cb2a8e17f0
Arjun Kamboj <arjunkamboj87699@gmail.com>
2026-09-13 17:32:20 +0530

    feat: integrate Tailwind CSS into Vite app and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import react from '@vitejs/plugin-react'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tanstackRouter({
-      target: 'react',
+      target: "react",
       autoCodeSplitting: true,
     }),
     react(),
+    tailwindcss(),
   ],
-})
+});
